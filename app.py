b272f0b7a29d0aa4f79da2537d974a606b09e01f
KaiPrince <34746763+KaiPrince@users.noreply.github.com>
2020-05-03 02:45:05 -0400

    Move routes to app.py

--- app.py
+++ app.py
@@ -7,8 +7,32 @@
 """
 
 
-from flask import Flask
+from itertools import islice
+
 import gunicorn
-import image_scraper
+from flask import Flask, redirect, render_template, request, url_for
+from app_service import collect_page_media, recursive_scrape
+from constants import recursion_depth_limit, recursion_spread_limit
+from service import clean_url, get_images, get_links, get_videos
 
 app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def request_form():
+    if request.method == "POST":
+        scrape_url = request.form["url"]
+        return redirect(url_for("results", source=scrape_url))
+
+    return render_template("index.html")
+
+
+@app.route("/scrape/<path:source>", methods=["GET", "POST"])
+def results(source):
+    url = clean_url(source)
+
+    collected_media = recursive_scrape(url, collect_page_media)
+
+    return render_template(
+        "results.html", source=source, collected_media=collected_media
+    )
--- image_scraper.py
+++ image_scraper.py
@@ -6,30 +6,3 @@
 * Description: This file contains a webscaper that gathers images and presents
 *  them in a paginated display.
 """
-
-from app import app
-from flask import render_template, request, redirect, url_for
-from constants import recursion_depth_limit, recursion_spread_limit
-from service import get_images, get_links, clean_url, get_videos
-from itertools import islice
-from app_service import recursive_scrape, collect_page_media
-
-
-@app.route("/", methods=["GET", "POST"])
-def request_form():
-    if request.method == "POST":
-        scrape_url = request.form["url"]
-        return redirect(url_for("results", source=scrape_url))
-
-    return render_template("index.html")
-
-
-@app.route("/scrape/<path:source>", methods=["GET", "POST"])
-def results(source):
-    url = clean_url(source)
-
-    collected_media = recursive_scrape(url, collect_page_media)
-
-    return render_template(
-        "results.html", source=source, collected_media=collected_media
-    )
