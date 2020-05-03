e4b783adfcc9a459e51fbe2f250e5853af3a0bd5
KaiPrince <34746763+KaiPrince@users.noreply.github.com>
2020-05-03 18:18:31 -0400

    Add Threading control. This stops the scraper when the user leaves the page.
    I don't like the way I implemented it, because I had to alter the original function to be thread dependent,  and I pass flags in to control it.
    I also used global vars. ugh.

--- app.py
+++ app.py
@@ -12,24 +12,45 @@ from image_scraper import views
 from flask_socketio import SocketIO, emit
 from image_scraper.sockets import scrape_and_emit
 
-app = Flask(__name__)
-socketio = SocketIO(app)
+from threading import Lock, Thread, Event
 
-app.add_url_rule("/", view_func=views.request_form, methods=["GET", "POST"])
+app = Flask(__name__)
+socketistop_flagadd_url_rule("/", view_func=views.request_form, methods=["GET", "POST"])
 app.add_url_rule(
     "/scrape/<path:source>", view_func=views.results, methods=["GET", "POST"]
 )
 
 
+# Controls background task
+scrape_thread: Thread = None
+scrape_thread_lock = Lock()
+sstop_flagd_stop = Event()
+
+
 @socketio.on("connect")
 def socket_connected():
     print("connected!")
+    scrape_thread_stop.clear()
 
 
 @socketio.on("start_scrape")
 def start_scrape(url):
-    if url:
-        scrape_and_emit(url)
+    if not url:
+        return
+
+    global scrape_thread
+    with scrape_thread_lock:
+        print("starting thread")
+        scrape_thread = socketio.start_background_task(
+            scrape_and_emit(url, scrape_thread_stop)
+        )
+
+
+@socketio.on("disconnect")
+def stop_scrape():
+    print("disconnected")
+    scrape_thread_stop.set()
+    scrape_thread.join()
 
 
 if __name__ == "__main__":
--- image_scraper/sockets.py
+++ image_scraper/sockets.py
@@ -11,13 +11,19 @@ from flask_socketio import emit
 from image_scraper.app_service import collect_page_media
 from image_scraper.app_service import recursive_scrape
 
+from threading import Event
 
-def scrape_and_emit(src):
+
+def scrape_and_emit(src: str, stop_flag: Event = None):
 
     for page, complete in recursive_scrape(src, collect_page_media):
         response = render_template(
             "components/media_page.html", page=page, complete=complete
         )
         emit("page", {"data": response})
+
+        if stop_flag and stop_flag.isSet():
+            print("stopping")
+            return
 
     emit("scrape-complete")
